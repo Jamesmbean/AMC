112ba775f97194f4e23c5a02b72ac32cc37c8f73
unknown <jbean@AMC5CG512168B.amc.local>
2015-06-08 15:48:54 -0600

    add files to repo

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+var from_element = document.getElementById ('1234');
+var vis = from_element.style;
+ 
+if (vis.display=='' || vis.display=='none')
+{
+    vis.display = 'block';
+}
+else
+{
+    vis.display = 'none';
+}
